8014cbfc8db13c6f2a681a095518ae8cbadca69e
Sonu Budidha <shravu800@gmail.com>
2025-12-30 06:29:27 +0530

    New thing

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-capstone-tfstate-fjkdhfjkdhfdkjhfkdjhf"
+    bucket         = "devops-capstone-tfstate-tuirytuirytruiyt"
     key            = "support-platform/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
